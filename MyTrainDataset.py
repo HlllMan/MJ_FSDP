61eaadb910853bdf671ad10d4b9837a9402e9061
Norman <1060150683@qq.com>
2026-02-09 16:52:52 +0800

    [Del] clean the old docker floder

--- MyTrainDataset.py
+++ MyTrainDataset.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import Dataset
-import torch.nn as nn
 
 class MyTrainDataset(Dataset):
     def __init__(self, size):
@@ -14,14 +13,5 @@ class MyTrainDataset(Dataset):
     def __getitem__(self, index):
         return self.data[index]
 
-#建立一个model 两层MLP
-class ToyModel(nn.Module):
-    def __init__(self):
-        super(ToyModel, self).__init__()
-        self.in_proj = nn.Linear(10, 32)
-        self.relu = nn.ReLU()
-        self.out_proj = nn.Linear(32, 5)
 
-    def forward(self, x):
-        return self.out_proj(self.relu(self.in_proj(x)))
         
